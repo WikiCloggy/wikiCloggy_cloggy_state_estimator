01bd097fd582206127f8d551e892570b6a58e6a9
이영기 <t9rres@ajou.ac.kr>
2018-06-14 19:21:31 +0900

    add simple net

---
+++ network/SimpleNet.py
@@ -0,0 +1,19 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from common import functions as function
+
+class SimpleNet:
+    def __init__(self):
+        #정규분포로 초기화
+        self.W = np.random.randn(2, 3)
+
+    def predict(self, X):
+        return np.dot(X, self.W)
+
+    def loss(self, X, T):
+        Z = self.predict(X)
+        Y = function.softmax(Z)
+        loss = function.CEE(Y, T)
+
+        return loss
